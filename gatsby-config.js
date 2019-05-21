80b85a1c4e1c67721eb7511248682572fd566067
Carolina Gomez Gilabert <carolgilabert@gmail.com>
2019-05-21 17:15:42 +0100

    Adding offline plugin to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,9 +81,7 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/images/favicon.png' // This path is relative to the root of the site.
       }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    },
+    'gatsby-plugin-offline'
   ]
 };
