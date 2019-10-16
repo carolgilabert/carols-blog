4dc6645f015a8fd025d8090d3f96262b2c2270c4
Carolina Gilabert <carolgilabert@gmail.com>
2019-10-16 18:55:47 +0100

    Pulled icons out of FA packages and deleted instagram link.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faAt } from '@fortawesome/free-solid-svg-icons';
 import { OutboundLink } from 'gatsby-plugin-google-analytics';
 import Box from '../Box';
-
-library.add(fab);
+import AtIcon from '../../images/at.svg';
+import TwitterIcon from '../../images/twitter.svg';
+import LinkedInIcon from '../../images/linkedin.svg';
+import GitLabIcon from '../../images/gitlab.svg';
 
 const StyledFooterWrapper = styled.div`
   margin: 20px 0px;
@@ -23,28 +21,21 @@ const StyledFooterWrapper = styled.div`
   }
 `;
 
-const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
-  margin: 0 20px;
-`;
-
-const Footer = ({ name, email, twitter, instagram, linkedin, gitlab }) => (
+const Footer = ({ name, email, twitter, linkedin, gitlab }) => (
   <StyledFooterWrapper>
     <Box px={[3, 3, 1]} textAlign="center" mt={20}>
       <span>{`Copyright © 2019 ${name}. Say hello 👋 `}</span>
       <OutboundLink href={email} target="_blank">
-        <StyledFontAwesomeIcon icon={faAt} />
+        <AtIcon />
       </OutboundLink>
       <OutboundLink href={twitter} target="_blank">
-        <StyledFontAwesomeIcon icon={['fab', 'twitter']} />
-      </OutboundLink>
-      <OutboundLink href={instagram} target="_blank">
-        <StyledFontAwesomeIcon icon={['fab', 'instagram']} />
+        <TwitterIcon />
       </OutboundLink>
       <OutboundLink href={linkedin} target="_blank">
-        <StyledFontAwesomeIcon icon={['fab', 'linkedin']} />
+        <LinkedInIcon />
       </OutboundLink>
       <OutboundLink href={gitlab} target="_blank">
-        <StyledFontAwesomeIcon icon={['fab', 'gitlab']} />
+        <GitLabIcon />
       </OutboundLink>
     </Box>
   </StyledFooterWrapper>
@@ -54,7 +45,6 @@ Footer.propTypes = {
   name: PropTypes.string,
   email: PropTypes.string,
   twitter: PropTypes.string,
-  instagram: PropTypes.string,
   gitlab: PropTypes.string,
   linkedin: PropTypes.string
 };
@@ -63,7 +53,6 @@ Footer.defaultProps = {
   name: 'Carolina Gilabert',
   email: 'mailto:carolgilabert@gmail.com',
   twitter: 'https://twitter.com/CarolSaysThings',
-  instagram: 'https://www.instagram.com/carolgilabert',
   gitlab: 'https://gitlab.com/carolgilabert',
   linkedin: 'https://www.linkedin.com/in/carolgilabert'
 };
