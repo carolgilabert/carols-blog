2e83703b1cce818f8d5d2e5c8cbdb87cf64447fd
Carolina Gomez Gilabert <carolgilabert@gmail.com>
2019-10-23 21:32:24 +0100

    Removing old footer tests.

--- src/components/Footer/footer.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Footer from './index';
-
-test('has the current year on the footer copyright', () => {
-    const tree = shallow(<Footer />);
-    const currentYear = new Date().getFullYear().toString();
-    expect(tree.text()).toEqual(expect.stringContaining(currentYear));
-});
