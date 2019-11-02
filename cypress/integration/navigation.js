82a3299b118bb08a500efc90226f2091335f7100
Carolina Gomez Gilabert <carolgilabert@gmail.com>
2019-11-02 10:17:22 +0000

    Adding cypress to codebase and one test file.

---
+++ cypress/integration/navigation.js
@@ -0,0 +1,28 @@
+context('Navigation', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it('should navigate to about', () => {
+        cy.get('a[href="/about"]').click();
+        cy.get('h1').should('have.text', 'About me');
+        cy.location().should(location => {
+            expect(location.pathname).to.eq('/about');
+        });
+    });
+    it('should navigate to blog', () => {
+        cy.get('a[href="/blog"]').click();
+        cy.get('h1').should('have.text', 'Blog');
+        cy.location().should(location => {
+            expect(location.pathname).to.eq('/blog');
+        });
+    });
+    it('should navigate to home', () => {
+        cy.visit('localhost:8000/blog');
+        cy.get('a[href="/"]').click();
+        cy.get('h1').should('have.text', `Hi! I'm Carol`);
+        cy.location().should(location => {
+            expect(location.pathname).to.eq('/');
+        });
+    });
+});
