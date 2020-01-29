7e4b0058eedc2e961454fd14d899ae2a546ad295
Carolina Gilabert <carolgilabert@gmail.com>
2020-01-29 10:54:07 +0000

    Removing GA.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,24 +9,6 @@ module.exports = {
     plugins: [
         'gatsby-plugin-react-helmet',
         {
-            resolve: `gatsby-plugin-google-analytics`,
-            options: {
-                trackingId: 'UA-126625629-1',
-                // Puts tracking script in the head instead of the body
-                head: false,
-                // Setting this parameter is optional
-                anonymize: true,
-                // Setting this parameter is also optional
-                respectDNT: true,
-                // Avoids sending pageview hits from custom paths
-                exclude: [],
-                // Any additional create only fields (optional)
-                sampleRate: 5,
-                siteSpeedSampleRate: 10,
-                cookieDomain: 'auto'
-            }
-        },
-        {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `images`,
