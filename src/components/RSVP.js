d64d5876e846db96569c46e47fe28a01c8eb36b6
Carolina Gomez Gilabert <carolgilabert@gmail.com>
2020-03-07 16:09:29 +0000

    Filled out RSVP component.

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const RSVP = () => <article className="h-entry">hello!</article>;
+const RSVP = ({ event, value }) => (
+    <article className="h-entry">
+        <section className="p-author h-card">
+            <a href="https://carolgilabert.me" className="u-url">
+                <img
+                    src="https://carolgilabert.me/images/avatar.jpeg"
+                    alt="my face"
+                    className="u-photo"
+                />
+                Carolina Gilabert
+            </a>
+        </section>
+        <span className="p-rsvp" value={value} />
+        <h2>
+            <a className="u-in-reply-to" href={event.url}>
+                {event.name}
+            </a>
+        </h2>
+        <time>{event.date}</time>
+    </article>
+);
+
+RSVP.propTypes = {
+    event: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired
+    }).isRequired,
+    value: PropTypes.oneOf(['yes', 'no', 'maybe', 'interested']).isRequired
+};
 
 export default RSVP;
