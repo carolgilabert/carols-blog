80eff84dc5266a064f765ce60dd20112b3a6a3c7
Carolina Gomez Gilabert <carolgilabert@gmail.com>
2020-03-10 17:57:41 +0000

    Fixed RSVP unit tests.

--- src/components/RSVP.test.js
+++ src/components/RSVP.test.js
@@ -15,52 +15,66 @@ describe('RSVP component', () => {
     );
 
     test('should have an article element with the entry class', () => {
-        expect(component.type()).toEqual('article');
+        expect(component.type().target).toEqual('article');
         expect(component.prop('className')).toEqual('h-entry');
     });
 
     test('should have a section with the author and card classes', () => {
-        expect(component.find('section').exists()).toBe(true);
-        expect(component.find('section').prop('className')).toEqual(
+        expect(component.find('RSVP__AuthorCard').exists()).toBe(true);
+        expect(component.find('RSVP__AuthorCard').prop('className')).toEqual(
             'p-author h-card'
         );
     });
 
     test('should have a link to my site with the url class within the section', () => {
-        expect(component.find('section a').exists()).toBe(true);
-        expect(component.find('section a').prop('href')).toEqual(
+        expect(component.find('RSVP__AuthorCard a').exists()).toBe(true);
+        expect(component.find('RSVP__AuthorCard a').prop('href')).toEqual(
             'https://carolgilabert.me'
         );
-        expect(component.find('section a').prop('className')).toEqual('u-url');
+        expect(component.find('RSVP__AuthorCard a').prop('className')).toEqual(
+            'u-url'
+        );
     });
 
     test('should have a photo of me with the photo class within the link', () => {
-        expect(component.find('section a img').exists()).toBe(true);
-        expect(component.find('section a img').prop('src')).toEqual(
+        expect(component.find('RSVP__AuthorCard a img').exists()).toBe(true);
+        expect(component.find('RSVP__AuthorCard a img').prop('src')).toEqual(
             'https://carolgilabert.me/images/avatar.jpeg'
         );
-        expect(component.find('section a img').prop('className')).toEqual(
-            'u-photo'
-        );
+        expect(
+            component.find('RSVP__AuthorCard a img').prop('className')
+        ).toEqual('u-photo');
     });
 
     test('should have a span element with the rsvp class and a value', () => {
-        expect(component.find('span').exists()).toBe(true);
-        expect(component.find('span').prop('className')).toEqual('p-rsvp');
-        expect(component.find('span').prop('value')).toEqual('yes');
+        expect(component.find('RSVP__ResponseIcon').exists()).toBe(true);
+        expect(component.find('RSVP__ResponseIcon').prop('className')).toEqual(
+            'p-rsvp'
+        );
+        expect(component.find('RSVP__ResponseIcon').prop('value')).toEqual(
+            'yes'
+        );
     });
 
     test('should have a header with a link to the event with the reply class', () => {
-        expect(component.find('h2 a').exists()).toBe(true);
-        expect(component.find('h2 a').prop('className')).toEqual(
-            'u-in-reply-to'
-        );
-        expect(component.find('h2 a').prop('href')).toEqual('some-event-url');
-        expect(component.find('h2 a').text()).toEqual('some event name');
+        expect(
+            component.find('RSVP__EventTitle RSVP__EventLink').exists()
+        ).toBe(true);
+        expect(
+            component.find('RSVP__EventTitle RSVP__EventLink').prop('className')
+        ).toEqual('u-in-reply-to');
+        expect(
+            component.find('RSVP__EventTitle RSVP__EventLink').prop('href')
+        ).toEqual('some-event-url');
+        expect(
+            component.find('RSVP__EventTitle RSVP__EventLink').text()
+        ).toEqual('some event name');
     });
 
     test('should have a time tag with the event date', () => {
-        expect(component.find('time').exists()).toBe(true);
-        expect(component.find('time').text()).toEqual('2020-03-14');
+        expect(component.find('RSVP__Time').exists()).toBe(true);
+        expect(component.find('RSVP__Time CalendarIcon').prop('date')).toEqual(
+            '2020-03-14'
+        );
     });
 });
