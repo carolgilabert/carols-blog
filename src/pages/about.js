ea49c8b8835e8ed27a9c01ed8600ebff95501411
Carolina Gilabert <carolgilabert@gmail.com>
2020-03-13 19:28:12 +0000

    Added about text.

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
 import PageLayout from '../components/PageLayout';
 import { ShadedH1 } from '../components/Misc';
 import Box from '../components/Box';
@@ -10,6 +11,13 @@ import Timeline from '../components/Timeline';
 const RoundAboutImg = styled(Img)`
     border-radius: 128px;
     border: solid ${({ theme }) => theme.textColour} 3px;
+    float: right;
+    margin: 10px;
+`;
+
+const AboutCard = styled.section`
+    max-width: 760px;
+    margin-bottom: 50px;
 `;
 
 const About = ({ data }) => {
@@ -21,12 +29,24 @@ const About = ({ data }) => {
             description="A little bit of information abour yours truly."
         >
             <Box
-                width={[1, 1, 1 / 2]}
+                width={[1, 1, 3 / 4]}
                 m={['3.5rem 0 0 0', '3.5rem 0 0 0', '3.5rem auto 0 auto']}
                 px={[3, 3, 0]}
             >
                 <ShadedH1>About me</ShadedH1>
-                <RoundAboutImg fixed={data.aboutImage.childImageSharp.fixed} />
+                <AboutCard>
+                    <RoundAboutImg
+                        fixed={data.aboutImage.childImageSharp.fixed}
+                    />
+                    Hey there! My name is Carol, I&apos;m a frontend software
+                    engineer. I currently work for Capital One, where I&apos;m
+                    part of the bestest team, Aviato. I&apos;m also an organiser
+                    at Tech Nottingham, Women in Tech and NottsJS. In my spare
+                    time I like to play videogames, swing dance, bake and play
+                    capoeira. I&apos;m also learning BSL, and how to play the
+                    guitar. You can scroll along for a whistle stop tour of my
+                    life so far:
+                </AboutCard>
                 <Timeline entries={allTimelineJson.nodes} />
             </Box>
         </PageLayout>
