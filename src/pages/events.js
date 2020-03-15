b455529989b83d10808bb95ad4ec8e6b328b7a04
Carolina Gilabert <carolgilabert@gmail.com>
2020-03-15 11:07:04 +0000

    Sourcing my rsvps from a json file.

--- src/pages/events.js
+++ src/pages/events.js
@@ -5,7 +5,7 @@ import Box from '../components/Box';
 import RSVP from '../components/RSVP';
 import { ShadedH1 } from '../components/Misc';
 
-const Events = () => (
+const Events = ({ data }) => (
     <PageLayout title="Events" description="The list of events I RSVP to.">
         <Box
             width={[1, 1, 1 / 2]}
@@ -13,28 +13,25 @@ const Events = () => (
             px={[3, 3, 0]}
         >
             <ShadedH1>Events</ShadedH1>
-            <RSVP
-                event={{
-                    id: 'indie-web-camp-london-2020',
-                    name: 'IndieWebCamp London 2020',
-                    url: 'https://2020.indieweb.org/london',
-                    date: '2020-03-14'
-                }}
-                value="yes"
-            />
-            <RSVP
-                event={{
-                    id: 'homewbrew-website-club-nottingham-april-2020',
-                    name:
-                        'Homebrew Website Club: Nottingham (Anniversary edition)',
-                    url:
-                        'https://events.indieweb.org/2020/03/homebrew-website-club-nottingham-anniversary-edition--Rcujt5SykHv1',
-                    date: '2020-03-18'
-                }}
-                value="yes"
-            />
+            {data.allRsvpsJson.nodes.map(event => (
+                <RSVP event={event} />
+            ))}
         </Box>
     </PageLayout>
 );
+
+export const pageQuery = graphql`
+    query EventsQuery {
+        allRsvpsJson(sort: { fields: date, order: DESC }) {
+            nodes {
+                name
+                url
+                value
+                date
+                id
+            }
+        }
+    }
+`;
 
 export default Events;
