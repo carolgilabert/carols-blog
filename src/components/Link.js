cad9653f33f620f73374c059609a2b6de9d84ae8
Carolina Gilabert <carolgilabert@gmail.com>
2020-04-23 20:56:45 +0100

    Simplifying link styles.

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,16 +2,14 @@ import styled, { css } from 'styled-components';
 import GatsbyLink from 'gatsby-link';
 
 export const LinkStyles = css`
-    text-decoration: none;
     color: inherit;
-    box-shadow: inset 0 -5px 0 var(--highContrastColour);
-    -webkit-transition: box-shadow 0.4s ease-in-out, color 0.4s;
-    transition: box-shadow 0.4s ease-in-out, color 0.4s;
-    padding: 3px;
+    text-decoration: underline solid var(--highContrastColour);
+    text-decoration-thickness: 2px;
+    transition: background-color 0.5s ease;
 `;
 
 export const LinkHoverStyles = css`
-    box-shadow: inset 0 -300px 0 var(--highContrastColour);
+    background-color: var(--highContrastColour);
     color: var(--accentColour);
 `;
 
