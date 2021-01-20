50db50bda8ad9821351f5902759544aa7e114f53
Carolina Gilabert <carolgilabert@gmail.com>
2021-01-20 17:52:54 +0000

    Actually removing page assets plugin from config.

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,8 @@
 const navigationPlugin = require("@11ty/eleventy-navigation");
-const pageAssetsPlugin = require("eleventy-plugin-page-assets");
 
 module.exports = function (eleventyConfig) {
     // Plugins
     eleventyConfig.addPlugin(navigationPlugin);
-    eleventyConfig.addPlugin(pageAssetsPlugin, {
-        mode: "parse",
-        postsMatching: "src/posts/*/*.md",
-    });
 
     // Copy
     eleventyConfig.addPassthroughCopy("src/images");
