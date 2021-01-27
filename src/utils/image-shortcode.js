533daff2dcee1c9ce32d8eef78a2fb6c2bdef1bd
Carolina Gilabert <carolgilabert@gmail.com>
2021-01-27 20:12:14 +0000

    Tweaking image sizes.

--- src/utils/image-shortcode.js
+++ src/utils/image-shortcode.js
@@ -2,7 +2,7 @@ const Image = require("@11ty/eleventy-img");
 
 module.exports = async (src, alt, sizes) => {
     let metadata = await Image(src, {
-        widths: [300, 600, 900, 1800],
+        widths: [600, 900],
         formats: ["avif", "webp", "jpeg"],
         urlPath: "/images/",
         outputDir: "./dist/images/",
