a8e99d5a754fc850fd715e7e6aa9d9e2a6133c71
Carolina Gilabert <carolgilabert@gmail.com>
2021-02-02 13:18:36 +0000

    Adding a SW to flush old Gatsby cache.

---
+++ src/service-worker.js
@@ -0,0 +1,14 @@
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches
+            .keys()
+            .then((allCaches) => {
+                return Promise.all(
+                    allCaches.map((cacheToDelete) => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+});
