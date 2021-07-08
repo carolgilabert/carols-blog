dba3d4233448bd6fcd21f8fe464041726eb74633
Carolina Gilabert <carolgilabert@gmail.com>
2021-07-08 21:47:22 +0100

    Fixed img reset and postcss issue.

--- src/utils/image-shortcode.js
+++ src/utils/image-shortcode.js
@@ -1,21 +1,27 @@
 const Image = require("@11ty/eleventy-img");
 
-module.exports = async (src, alt, sizes) => {
-    let metadata = await Image(src, {
-        widths: [600, 900],
-        formats: ["avif", "webp", "jpeg"],
-        urlPath: "/images/",
-        outputDir: "./dist/images/",
-    });
+module.exports = async (src, alt, sizes, caption) => {
+  let metadata = await Image(src, {
+    widths: [600, 900],
+    formats: ["avif", "webp", "jpeg"],
+    urlPath: "/images/",
+    outputDir: "./dist/images/",
+  });
 
-    let imageAttributes = {
-        alt,
-        sizes,
-        loading: "lazy",
-        decoding: "async",
-    };
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
 
-    return Image.generateHTML(metadata, imageAttributes, {
-        whitespaceMode: "inline",
-    });
+  let pictureHtml = Image.generateHTML(metadata, imageAttributes, {
+    whitespaceMode: "inline",
+  });
+
+  if (caption) {
+    pictureHtml += `<figcaption>${caption}</figcaption>`;
+  }
+
+  return `<figure>${pictureHtml}</figure>`;
 };
