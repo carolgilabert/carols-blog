c7982eb77c07e13c2ec8ac16d2abeecce2b38654
Carolina Gomez Gilabert <hello@carol.gg>
2022-03-27 09:15:54 +0100

    Add talks page

---
+++ src/data/talks.js
@@ -0,0 +1,61 @@
+module.exports = [
+    {
+        title: 'DevOps vs SRE',
+        date: '2018-10-18',
+        event: 'Notts IoT',
+        description: 'Some description goes here.',
+        links: {
+            slides: 'https://talks.carol.gg/decks/devops-vs-sre/',
+        }
+    },
+    {
+        title: 'Panic Driven Development',
+        date: '2019-09-26',
+        event: 'DevOpsDays London 2019',
+        description: 'Some description goes here.',
+        links: {
+            video: 'https://youtu.be/yVWNuv_j05k',
+            slides: 'https://talks.carol.gg/decks/panic-driven-development/'
+        }
+    },
+    {
+        title: 'Test yourself before you wreck yourself',
+        date: '2019-11-07',
+        event: 'Women in Tech Nottingham',
+        description: 'Some description goes here.',
+        links: {
+            slides: 'https://talks.carol.gg/decks/test-yourself/'
+        }
+    },
+    {
+        title: 'How to train your impostor',
+        date: '2020-01-31',
+        event: 'NDC London',
+        description: 'Some description goes here.',
+        links: {
+            slides: 'https://talks.carol.gg/decks/train-your-impostor/',
+            video: 'https://youtu.be/if5YLYRIcc8',
+            post: '/blog/impostor-syndrome/'
+        }
+    },
+    {
+        title: 'Women in Tech: what do we even do?',
+        date: '2020-06-30',
+        event: 'DevOps Notts',
+        description: 'Some description goes here.',
+        links: {
+            slides: 'https://talks.carol.gg/decks/women-in-tech-what-do-we-even-do/',
+            video: 'https://youtu.be/nxecjx-aEHw?t=8421'
+        }
+    },
+    {
+        title: 'Looking for a job while looking after yourself',
+        date: '2022-02-03',
+        event: 'Women in Tech Nottingham',
+        description: 'Some description goes here.',
+        links: {
+            slides: 'https://talks.carol.gg/decks/looking-for-a-job-while-looking-after-yourself.pdf',
+            post: '/blog/looking-for-a-job/'
+        }
+    }
+]
